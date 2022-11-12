ed2e4e21c69448800bb815e7c54d53d21720f7ab
Dav1Samu3l <davidev.front@gmail.com>
2022-11-11 22:06:50 -0300

    m

--- script.js
+++ script.js
@@ -5,26 +5,24 @@
 
 
 function operacao() {
-    let inputs = document.querySelectorAll('.input')
-    let result = document.querySelector(".resultado")
-    let select = document.querySelector('.select')
-    let value = inputs
-    let index_select = select.options.selectedIndex
+    const inputs = document.querySelectorAll('.input')
+    const result = document.querySelector(".resultado")
+    const select = document.querySelector('.select')
+    const value_input = inputs
+    const index_select = select.options.selectedIndex
 
     if (select.options[index_select].innerHTML == '+') {
-        result.innerText = parseInt(value[0]) + parseInt(value[1])
+        result.innerText = parseInt(value_input[0]) + parseInt(value_input[1])
     } else {
 
     }
 }
 
-
 function calcular() {
-    let button = document.getElementById('calcular')
+    var elemento = document.getElementById('calcular')
 
     button.addEventListener('click', () => {
         operacao()
     })
 }
 calcular()
-console.log(operacao());
